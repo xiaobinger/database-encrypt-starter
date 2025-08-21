c3943db2fb95c91509541f4a169339d8ca0d3c52
熊兵 <xiongbing@juheba.com>
2025-08-21 15:40:28 +0800

    fix:数据库加密存储

--- src/main/java/com/database/config/DatabaseEncryptAutoConfig.java
+++ src/main/java/com/database/config/DatabaseEncryptAutoConfig.java
@@ -1,24 +1,27 @@
 package com.database.config;
 
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.database.interceptor.MyBatisEncryptionInterceptor;
 import com.database.typeHandler.FieldTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author xiongbing
  * @date 2025/8/15 13:40
  * @description 拦截器自动配置
  */
-public class InterceptorAutoConfig {
+@Configuration
+@ComponentScan(basePackages = "com.database")
+public class DatabaseEncryptAutoConfig {
 
     /**
      * @see com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration
      */
     @Bean
-    ConfigurationCustomizer configurationCustomizer(MybatisPlusInterceptor mybatisPlusInterceptor) {
+    ConfigurationCustomizer databaseEncryptConfigurationCustomizer() {
         return configuration -> {
             configuration.getTypeHandlerRegistry().register(String.class, JdbcType.VARCHAR, new FieldTypeHandler());
             configuration.addInterceptor(new MyBatisEncryptionInterceptor());
